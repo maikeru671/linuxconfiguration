ca80ecf6659bdd8d8a2beb5581f16649570e8bf6
Mike Cruz <okitnam89@gmail.com>
2024-09-02 21:26:20 -0700

    updated install-ansible.sh

--- install-ansible.sh
+++ install-ansible.sh
@@ -4,6 +4,6 @@ sudo apt-get update -y
 
 sudo apt-get install ansible -y
 
-ansible-pull -U git@github.com:maikeru671/linuxconfiguration cofigure.yml
+ansible-pull -U git@github.com:maikeru671/linuxconfiguration netplan-playbook.yaml
 
 
